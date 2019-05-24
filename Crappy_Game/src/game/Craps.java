d1fefff2ef2f4764bebbedfeafd27d95d7a97f21
Jared Cortez <jared8cortez@gmail.com>
2019-05-24 16:44:59 -0500

    Basic game and test setup

--- Crappy_Game/src/game/Craps.java
+++ Crappy_Game/src/game/Craps.java
@@ -1,10 +1,70 @@
 package game;
 
 public class Craps {
-
+	static int randomNumber() {
+		Random dice = new Random();
+		int myNum = dice.nextInt(6)+1;
+		System.out.println(myNum);
+		return myNum;
+		}
+	
+	static int adder(int a, int b) {
+		return a+b;
+	}
+	
+	static int rollDice() {
+		int dice1 = randomNumber();
+		int dice2 = randomNumber();
+		int rollTotal = adder(dice1, dice2);
+		System.out.println(rollTotal);
+		return rollTotal;
+		}
+	
+	static void playGame() {
+		int firstRoll = rollDice();
+		
+		if(firstRoll == 7) {
+			System.out.println("win");
+		}
+		
+		else if(firstRoll == 11) {
+			System.out.println("win");
+		}
+		
+		else {
+			int point = firstRoll;
+			System.out.println("point is " + point);
+			
+			int newRoll = rollDice();
+			System.out.println("new roll was " + newRoll);
+			
+			boolean hitPoint = false;
+			
+			if(newRoll == point) {
+				hitPoint = true;	
+				System.out.println("you win!");
+				} 
+			else if(newRoll == 7) {
+				hitPoint = true;
+				System.out.println("You lose");
+			}
+				
+				else {
+					while(hitPoint = false) {
+						int newestRoll = rollDice();
+					}
+				}
+				
+			
+					
+				
+			}
+	}
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-	}
+		playGame();
+		}
 
 }
--- Crappy_Game/src/tests/Test.java
+++ Crappy_Game/src/tests/Test.java
@@ -4,9 +4,62 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class Test {
 
-	@org.junit.jupiter.api.Test
-	void test() {
-		fail("Not yet implemented");
-	}
-
+	int adder(int a, int b){
+		return a+b;
+		}
+		@Test
+		void test() {
+		assertEquals(2, adder(1,1));
+		
+		//fail("Not yet implemented");
+		}
+		//////////////////////////////////////////////////
+		int randomNumber() {
+		Random dice = new Random();
+		int myNum = dice.nextInt(6)+1;
+		System.out.println(myNum + " random number");
+		return myNum;
+		}
+		@Test
+		public void randomNumberTest(){
+			int random = randomNumber();
+			int high = 6;
+			int low = 1;
+			assertTrue(high >= random);
+			assertTrue(low  <= random);
+		}
+		/////////////////////////////////////////////////
+		private int rollDice() {
+			int dice1 = randomNumber();
+			int dice2 = randomNumber();
+			int rollTotal = adder(dice1, dice2);
+			return rollTotal;
+		}
+		@Test
+		public void rollTest(){
+			int random = rollDice();
+			int high = 12;
+			int low = 2;
+			assertTrue(high >= random);
+			assertTrue(low  <= random);
+		}
+		//////////////////////////////////////////////////
+		@Test
+		public void crapOut(){
+			int random = rollDice();
+			int bad1 = 2;
+			int bad2 = 3;
+			int bad3 = 12;
+			System.out.println("This crapout " + random);
+			assertFalse(random == bad1 || random == bad2 || random == bad3);
+		}
+		
+		@Test
+		public void crapIn() {
+			int random = rollDice();
+			int good1 = 7;
+			int good2 = 11;
+			System.out.println("this crapin " + random);
+			assertFalse(random == good1 || random == good2);
+		}	
 }
