d82877542c73216ca36148d61b55678392bb0edc
Kyle <foscat4o1k@outlook.com>
2019-05-24 20:39:22 -0500

    added some comments

--- Crappy_Game/src/game/Craps.java
+++ Crappy_Game/src/game/Craps.java
@@ -1,67 +1,106 @@
 package game;
 
 public class Craps {
-	static int randomNumber() {
+	/**
+	 * randomNumber - Function should serve as a repeatable function as a roll of a single die
+	 * dice - Declares a new Random generator 
+	 * die - A random number between 1 & 6 
+	 */
+	static int rollOne() {
 		Random dice = new Random();
-		int myNum = dice.nextInt(6)+1;
-		System.out.println(myNum);
-		return myNum;
-		}
+		int die = dice.nextInt(6)+1;
+		System.out.println(die);
+		return die;
+	}
 	
+	/**
+	 * adder - Takes in 2 arguments that are int and will return the sum of those 2 numbers
+	 */
 	static int adder(int a, int b) {
 		return a+b;
 	}
 	
+	/**
+	 * rollDice - Simulate dice throw and return the sum of the dice 
+	 * dice1 & 2 - Call single roll function to get a random value and hold value in varible
+	 * rollTotal - Call add function to get sum of this dice throw value
+	 */
 	static int rollDice() {
-		int dice1 = randomNumber();
-		int dice2 = randomNumber();
+		int dice1 = rollOne();
+		int dice2 = rollOne();
 		int rollTotal = adder(dice1, dice2);
 		System.out.println(rollTotal);
 		return rollTotal;
-		}
-	
+	}
+
+	/**
+	 * playGame - Simulate a game of craps
+	 * firstRoll - Call functionto rool dice and hold that value
+	 *  
+	 */
 	static void playGame() {
 		int firstRoll = rollDice();
 		
+		//If the first roll is 7 the user automatically wins
 		if(firstRoll == 7) {
-			System.out.println("win");
+			System.out.println("Lucky number 7! You won!");
 		}
-		
+		//If the first roll is 11 the user automatically wins
 		else if(firstRoll == 11) {
-			System.out.println("win");
+			System.out.println("Wow you won on the first roll!");
 		}
-		
+		//If first roll equals 2 user automatically loses
+		else if(firstRoll == 2){
+			System.out.println("Aww shucks snake eyes.. You lose.")
+		}
+		//If first roll equals 2 user automatically loses
+		else if(firstRoll == 3){
+			System.out.println("A 3 is no good here bucco! You lose.")
+		}
+		//If first roll equals 2 user automatically loses
+		else if(firstRoll == 12){
+			System.out.println("Double sixes huh? Well you lose.")
+		}
+		//If user does not win or lose on first roll
 		else {
+
+			//Hold value of firstRoll as point for the rest of game
 			int point = firstRoll;
-			System.out.println("point is " + point);
+			System.out.println("Point is: " + point);
 			
+			//Call function to throw dice
 			int newRoll = rollDice();
-			System.out.println("new roll was " + newRoll);
+			System.out.println("New roll was: " + newRoll);
 			
+			//Local varible to control while loop
 			boolean hitPoint = false;
 			
+			//If the new roll value equals the point user wins
 			if(newRoll == point) {
 				hitPoint = true;	
-				System.out.println("you win!");
-				} 
+				System.out.println("Two for the money! You win!");
+			} 
+			//If the new roll value equals 7 the user loses
 			else if(newRoll == 7) {
 				hitPoint = true;
-				System.out.println("You lose");
+				System.out.println("You lose.. 7 must not be that lucky..:(");
 			}
-				
-				else {
-					while(hitPoint = false) {
-						int newestRoll = rollDice();
-					}
+			/**
+			 * If player does not win or lose on second roll
+			 * Under Construction
+			 * Needs: A while loop to keep throwing dice as long as hitPoint is false
+			 */
+			else {
+				while(hitPoint = false) {
+					int newestRoll = rollDice();
 				}
-				
-			
-					
-				
 			}
-	}
+
+		} // End first roll else
+
+	} //End playGame function
 	
-	
+	//Test function chains and run game here
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		playGame();
--- Crappy_Game/src/tests/Test.java
+++ Crappy_Game/src/tests/Test.java
@@ -4,62 +4,89 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class Test {
 
+	//Write function to add 2 numbers together
 	int adder(int a, int b){
 		return a+b;
-		}
-		@Test
-		void test() {
+	}
+	//Write test to give framework for add function
+	@Test
+	void test() {
 		assertEquals(2, adder(1,1));
-		
-		//fail("Not yet implemented");
-		}
-		//////////////////////////////////////////////////
-		int randomNumber() {
+	}
+	//////////////////////////////////////////////////
+
+	//Write function to return a random number between 1 & 6
+	int randomNumber() {
+		//Declare a new Random constructor
 		Random dice = new Random();
+		//Make random be a whole number and between 1 & 6
 		int myNum = dice.nextInt(6)+1;
 		System.out.println(myNum + " random number");
 		return myNum;
-		}
-		@Test
-		public void randomNumberTest(){
-			int random = randomNumber();
-			int high = 6;
-			int low = 1;
-			assertTrue(high >= random);
-			assertTrue(low  <= random);
-		}
-		/////////////////////////////////////////////////
-		private int rollDice() {
-			int dice1 = randomNumber();
-			int dice2 = randomNumber();
-			int rollTotal = adder(dice1, dice2);
-			return rollTotal;
-		}
-		@Test
-		public void rollTest(){
-			int random = rollDice();
-			int high = 12;
-			int low = 2;
-			assertTrue(high >= random);
-			assertTrue(low  <= random);
-		}
-		//////////////////////////////////////////////////
-		@Test
-		public void crapOut(){
-			int random = rollDice();
-			int bad1 = 2;
-			int bad2 = 3;
-			int bad3 = 12;
-			System.out.println("This crapout " + random);
-			assertFalse(random == bad1 || random == bad2 || random == bad3);
-		}
-		
-		@Test
-		public void crapIn() {
-			int random = rollDice();
-			int good1 = 7;
-			int good2 = 11;
-			System.out.println("this crapin " + random);
-			assertFalse(random == good1 || random == good2);
-		}	
+	}
+	//Write test to ensure random number can only be 1-6
+	@Test
+	public void randomNumberTest(){
+		//Get random number
+		int random = randomNumber();
+		//Make limiters for acceptable range of values
+		int high = 6;
+		int low = 1;
+		assertTrue(high >= random);
+		assertTrue(low  <= random);
+	}
+	/////////////////////////////////////////////////
+
+	//Write function to simuslate dice roll using random number function and add number function
+	private int rollDice() {
+		//Call random number to get roll value of each die
+		int dice1 = randomNumber();
+		int dice2 = randomNumber();
+		//Call add function to get sum of dice thrown
+		int rollTotal = adder(dice1, dice2);
+		return rollTotal;
+	}
+	//Write test to simulate dice roll and check that values can only add up to acceptable range
+	@Test
+	public void rollTest(){
+		//Get dice roll
+		int random = rollDice();
+		//Make limiters for acceptable range of values
+		int high = 12;
+		int low = 2;
+		assertTrue(high >= random);
+		assertTrue(low  <= random);
+	}
+	//////////////////////////////////////////////////
+
+	//Write function to see if user lost in first roll
+
+	//Write test to check if player crapped out on first roll
+	@Test
+	public void crapOut(){
+		//Get dice roll
+		int random = rollDice();
+		//Make limiters for acceptable range of values
+		int bad1 = 2;
+		int bad2 = 3;
+		int bad3 = 12;
+		System.out.println("This crapOut: " + random);
+		assertFalse(random == bad1 || random == bad2 || random == bad3);
+	}
+	///////////////////////////////////////////////////////////////////
+
+	//Write function to check for first roll win 
+
+	//Write test to check if player won the game in the first round
+	@Test
+	public void crapIn() {
+		//Get dice roll
+		int random = rollDice();
+		//Make limiters for acceptable range of values
+		int good1 = 7;
+		int good2 = 11;
+		System.out.println("This crapIn: " + random);
+		assertFalse(random == good1 || random == good2);
+	}	
+	////////////////////////////////////////////////////////////////////
 }
